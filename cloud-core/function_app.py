87c398800c40123e63fadd19f86244fb774bc5d0
C. Tinkelrot <chavit@skyvar.co.il>
2024-01-25 10:16:11 +0200

    try

--- cloud-core/function_app.py
+++ cloud-core/function_app.py
@@ -10,8 +10,7 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     try:
-        # inspection_process_management("moon",3)
-        logging.warn("hello")
+        inspection_process_management("moon",3)
 
     except Exception as e:
         logging.info(e)
