aac0a99f87d517a4ad86f73edad80f59694be96d
C. Tinkelrot <chavit@skyvar.co.il>
2024-03-03 14:48:33 +0200

    deploy

--- services/users/func-try-user-disable-automation/function_app.py
+++ services/users/func-try-user-disable-automation/function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
 import logging
 
-from project.process_management import inspection_process_management
+# from project.process_management import inspection_process_management
 
 app = func.FunctionApp()
 
@@ -10,7 +10,7 @@ app = func.FunctionApp()
 @app.route(route="")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    inspection_process_management()
+    # inspection_process_management()
     
     
 
